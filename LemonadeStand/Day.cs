a16cd3acff9daf27aef442306a6ddffe28b944cf
ehenderson421 <ehenderson421@gmail.com>
2018-03-30 07:34:14 -0500

    Add project files.

---
+++ LemonadeStand/Day.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Day
+    {
+        // Member Variables
+
+
+
+        //Constructor
+
+
+
+        //Member Methods
+
+
+
+
+    }
+}
---
+++ LemonadeStand/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// data source = DESKTOP-V8K07HH integrated security = SSPI
+namespace LemonadeStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
